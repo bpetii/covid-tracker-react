378231a2415db986ad4ad2010eee91326ae3c742
Peder Aalen <pederwaa@ifi.uio.no>
2020-10-14 20:03:55 +0200

    feat: init api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { DataQuery } from '@dhis2/app-runtime'
+import { useDataQuery } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
 import classes from './App.module.css'
 
@@ -8,24 +9,36 @@ const query = {
         resource: 'me',
     },
 }
+const queryClusters = {
+    trackedEntityInstances: {
+        resource: 'trackedEntityInstances',
+        params: {
+            paging: 'false',
+            ou: 'uoPrVFsvJiY',
+            fields: ['orgUnit', 'lastUpdated', 'created'],
+        },
+    },
+}
 
-const MyApp = () => (
-    <div className={classes.container}>
-        <DataQuery query={query}>
-            {({ error, loading, data }) => {
-                if (error) return <span>ERROR</span>
-                if (loading) return <span>...</span>
-                return (
-                    <>
-                        <h1>
-                            {i18n.t('Hello {{name}}', { name: data.me.name })}
-                        </h1>
-                        <h3>{i18n.t('Welcome to DHIS2!')}</h3>
-                    </>
-                )
-            }}
-        </DataQuery>
-    </div>
-)
+const MyApp = () => {
+    const { error, loading, data } = useDataQuery(queryClusters)
+    console.log('data', data)
+    return (
+        <div className={classes.container}>
+            <DataQuery query={query}>
+                {({ error, loading, data }) => {
+                    if (error) return <span>ERROR</span>
+                    if (loading) return <span>...</span>
+                    return (
+                        <>
+                            <h1>{i18n.t('Hello {{name}}')}</h1>
+                            <h3>{i18n.t('Welcome to DHIS2!')}</h3>
+                        </>
+                    )
+                }}
+            </DataQuery>
+        </div>
+    )
+}
 
 export default MyApp
